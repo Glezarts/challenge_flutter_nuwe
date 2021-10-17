04f16776bcc1ff1c785bd477ff90cbf1ceb4635e
Glezarts <evaglezart@gmail.com>
2021-10-18 00:04:33 +0200

    change colors
    Signed-off-by: Glezarts <evaglezart@gmail.com>

--- lib/src/configuration/app_styles.dart
+++ lib/src/configuration/app_styles.dart
@@ -7,9 +7,6 @@ class AppStyles {
   static const Color backgroundDark = Color(0xff232730);
   static const Color primaryColor = Color(0xff569B51);
   static const Color colorTextPrimary = Color(0xffF0F0F0);
-  static const Color focusNav = Color(0xff43474C);
-  static const Color textColorNonFocus = Color(0xff757575);
-  static const Color textColorRed = Color(0xffE80D0D);
   static const LinearGradient gradientButton = LinearGradient(
     colors: [
       Color(0xffFACFFC8),
